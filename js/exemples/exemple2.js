8435846f0b77ad311bdb27b7f5bdfa8e3faa4067
VPI <VPI@FLWLATIT4002.flw.grp>
2015-10-21 16:58:28 +0200

    Merge origin/master
    Conflicts:
    	Hypo-Java/src/be/vinnyroundfoot/hypo/Exemples.java
    	Hypo-Java/src/be/vinnyroundfoot/hypo/Hypo.java

--- js/exemples/exemple2.js
+++ js/exemples/exemple2.js
@@ -7,10 +7,11 @@ $(function(){
         var $f = $('#frmExemple2'),
             K0 = $f.find('#K0_2').val(),
             n = $f.find('#n_2').val(),
+            n_amd = $f.find('#n_2_amd').val(),
             t = $f.find('#t_2').val(),
             dec = 2;
         
-        var res = Hypo.VPM(K0, n, t, dec);
+        var res = Hypo.VPM(K0, n, t, n_amd, dec);
         $f.find('#res_2').val(res);
     });
     
